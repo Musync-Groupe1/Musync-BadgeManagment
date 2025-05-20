202ee7c2aa9a2a252cbe7c26eaf90c5b0228b8e2
mysthic <mouloud.messd@gmail.com>
2025-05-20 16:54:13 +0200

    feature/configuration de keyclock et route guard avec keycloack

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,44 @@ import { Module } from '@nestjs/common';
 import { BadgeModule } from './badge.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { KafkaModule } from './kafka/kafka.module';
+import {
+  KeycloakConnectModule,
+  ResourceGuard,
+  RoleGuard,
+  AuthGuard,
+} from 'nest-keycloak-connect';
+import { AppService } from './app.service';
+import { APP_GUARD } from '@nestjs/core';
+
 @Module({
-  imports: [BadgeModule, PrismaModule, KafkaModule],
+  imports: [
+    BadgeModule,
+    PrismaModule,
+    KafkaModule,
+    KeycloakConnectModule.register({
+      authServerUrl: process.env.KEYCLOACK_URL,
+      realm: process.env.KEYCLOACK_REALM,
+      clientId: process.env.KEYCLOACK_CLIENT_ID,
+      secret: process.env.KEYCLOACK_SECRET!,
+    }),
+  ],
+  providers: [
+    AppService,
+
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+  ],
 })
 export class AppModule {}
